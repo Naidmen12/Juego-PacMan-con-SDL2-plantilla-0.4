f264cf056da80b58dfed52f6a87667ef5222f824
Naidmen12 <christianyesidcf@ufps.edu.co>
2025-02-18 22:38:59 -0500

    Add files via upload

---
+++ x64/Debug/Contenido/TMapa.tsx
@@ -0,0 +1,16 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="TMapa" tilewidth="32" tileheight="32" tilecount="2" columns="2">
+ <image source="TMapa.png" width="64" height="32"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="3">
+   <object id="2" x="7" y="0.25" width="18.75" height="30.875">
+    <ellipse/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="1">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+</tileset>
